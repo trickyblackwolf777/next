df5ded8afb8b4eafd5bdb0e70dcc5c0f37a44562
ConstellationOfLEM <little.maclion@gmail.com>
2026-04-28 09:48:33 +0000

    fix: enable logout button in header

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-screen flex flex-col">
-        <Header title="My App" />
+        <Header title="My App" showLogout={true} />
         <main className="flex-1">{children}</main>
         <Footer links={footerLinks} companyName="My Next.js App" />
       </body>
